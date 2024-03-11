e36da9b32f7acd0627763ea3ee17215b850b339c
Ruben van Voorden <rubenvanvoorden@me.com>
2024-03-11 14:53:07 +0100

    Make game ask if the user want's to play again. Make game validate if the user's input is within min number through max number. Make game display user's run after finishing it.

--- Gokje.java
+++ Gokje.java
@@ -3,48 +3,133 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Gokje {
-    static Random random = new Random();
+    public static void main(String[] args) {
+        do {
+            Game game = new Game(0, 15, 7);
+            game.play();
+            System.out.println("Would you like to play again? (y/n)");
+        } while(UserInput.readYesOrNo());
+        System.out.println("Ok, goodbye!");
+    }
+}
+
+class UserInput {
     static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        System.out.println("hello gokje");
+    public static boolean readYesOrNo() {
+        while(true) {
+            String input = scanner.nextLine();
 
-        Game(0, 15, 7);
+            switch(input) {
+                case "y":
+                    return true;
+                case "n":
+                    return false;
+                default:
+                    System.out.println("Please enter 'y' or 'n'.");
+                    break;
+            }
+        }
     }
 
-    private static int _readNumberFromUser() {
+    public static int readNumberFromUser() {
         while(true) {
             String input = scanner.nextLine();
             try {
                 return Integer.parseInt(input);
             }
             catch (java.lang.NumberFormatException e) {
-                System.out.println(String.format("%s is not a whole number.", input));
+                System.out.println(String.format(
+                    "%s is not a whole number.", input
+                ));
             }
         }
     }
+}
 
-    public static void Game(int low, int high, int maxTries) {
-        int secret = random.nextInt(high - low + 1) + low;
-        int guess;
-        ArrayList<Integer> guesses = new ArrayList<Integer>();
-        while(true) {
+class Game {
+    int minNum;
+    int maxNum;
+    int maxTries;
+    int secret;
+    ArrayList<Integer> tries;
+
+    static Random random = new Random();
+
+    Game(int minNum, int maxNum, int maxTries, int secret) {
+        this.minNum = minNum;
+        this.maxNum = maxNum;
+        this.maxTries = maxTries;
+        this.secret = secret;
+        tries = new ArrayList<Integer>();
+    }
+
+    Game(int minNum, int maxNum, int maxTries) {
+        this(
+            minNum, maxNum, maxTries,
+            random.nextInt(maxNum - minNum + 1) + minNum
+        );
+    }
+
+    private boolean numberWithinBounds(int num) {
+        return num >= minNum & num <= maxNum;
+    }
+
+    private int getNextGuess() {
+        int num;
+        do {
             System.out.println(String.format(
-                "Please guess a number between %d and %d", low, high
+                "Please enter a number from %d thru %d.", minNum, maxNum
             ));
+            num = UserInput.readNumberFromUser();
+        } while(!numberWithinBounds(num));
+        return num;
+    }
 
-            guess = _readNumberFromUser();
-            guesses.add(guess);
+
+    private void printRun() {
+        System.out.println("Here is an overview of your run:");
+        for(int gues : tries) {
+            System.out.println(gues);
+        }
+    }
+
+    public void play() {
+        while(true) {
+            int triesLeft = maxTries - tries.size();
+            System.out.println(String.format(
+                "You have %d guess%s left.",
+                triesLeft, triesLeft != 1 ? "es" : "")
+            );
+            int guess = getNextGuess();
 
             if (guess == secret) {
                 System.out.println("You guessed it!");
+                tries.add(guess);
+                printRun();
                 return;
             }
-            if (guesses.size() == maxTries) {
-                System.out.println(String.format("You lost! The secret number was: %d.", secret));
+
+            boolean contains = tries.contains(guess);
+            tries.add(guess);
+
+            if (contains) {
+                System.out.println(String.format(
+                    "You have already tried %d!", guess
+                ));
+            }
+
+            if (tries.size() == maxTries) {
+                System.out.println(String.format(
+                    "You lost! The secret number was: %d.", secret)
+                );
+                printRun();
                 return;
+            }
+
+            if (!contains) {
+                System.out.println("That's not it!");
             }
         }
-
     }
 }
